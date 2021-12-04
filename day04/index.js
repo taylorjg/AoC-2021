b9398e6ce567c8e560063a165ef5a307ebd75729
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-04 09:39:58 +0000

    Day 4 (parts 1 and 2)

--- day04/index.js
+++ day04/index.js
@@ -1,15 +1,71 @@
 const fs = require('fs').promises
-const R = require('rambda')
+const R = require('ramda')
 
-const part1 = (numbers, boards) => {
-  console.log('Answer (part1):', 0)
-}
+const sum = xs => xs.reduce((a, b) => a + b, 0)
 
 class Board {
   constructor(lines) {
-    this.lines = lines
-    this.rows = this.lines.map(line => line.split(/\s+/).map(Number))
-    this.cols = R.range(0, 5).map(col => this.rows.map(row => row[col]))
+    const rowsOfNumbers = lines.map(line => line.split(/\s+/).map(Number))
+    const colsOfNumbers = R.range(0, 5).map(col => rowsOfNumbers.map(row => row[col]))
+    this.rowsOfCells = rowsOfNumbers.map(rowOfNumbers => rowOfNumbers.map(number => ({ number, marked: false })))
+    this.colsOfCells = colsOfNumbers.map(colOfNumbers => colOfNumbers.map(number => ({ number, marked: false })))
+  }
+
+  reset() {
+    for (const rowOfCells of this.rowsOfCells) {
+      for (const cell of rowOfCells) {
+        cell.marked = false
+      }
+    }
+    for (const colOfCells of this.colsOfCells) {
+      for (const cell of colOfCells) {
+        cell.marked = false
+      }
+    }
+
+  }
+
+  markOff(number) {
+    for (const rowOfCells of this.rowsOfCells) {
+      for (const cell of rowOfCells) {
+        if (cell.number === number) {
+          cell.marked = true
+        }
+      }
+    }
+    for (const colOfCells of this.colsOfCells) {
+      for (const cell of colOfCells) {
+        if (cell.number === number) {
+          cell.marked = true
+        }
+      }
+    }
+  }
+
+  get won() {
+    for (const rowOfCells of this.rowsOfCells) {
+      if (rowOfCells.every(cell => cell.marked)) {
+        return true
+      }
+    }
+    for (const colOfCells of this.colsOfCells) {
+      if (colOfCells.every(cell => cell.marked)) {
+        return true
+      }
+    }
+    return false
+  }
+
+  get unmarkedNumbers() {
+    const numbers = []
+    for (const rowOfCells of this.rowsOfCells) {
+      for (const cell of rowOfCells) {
+        if (!cell.marked) {
+          numbers.push(cell.number)
+        }
+      }
+    }
+    return numbers
   }
 }
 
@@ -20,11 +76,40 @@ const parseLines = lines => {
   return { numbers, boards }
 }
 
+const part1 = (numbers, boards) => {
+  for (const number of numbers) {
+    for (const board of boards) {
+      board.markOff(number)
+      if (board.won) {
+        console.log('Answer (part1):', sum(board.unmarkedNumbers) * number)
+        return
+      }
+    }
+  }
+}
+
+const part2 = (numbers, boards) => {
+  let remainingBoards = boards
+  for (const number of numbers) {
+    for (const board of remainingBoards) {
+      board.markOff(number)
+    }
+    const remainingBoards2 = remainingBoards.filter(board => !board.won)
+    if (remainingBoards.length === 1 && remainingBoards2.length === 0) {
+      const board = remainingBoards[0]
+      console.log('Answer (part2):', sum(board.unmarkedNumbers) * number)
+    }
+    remainingBoards = remainingBoards2
+  }
+}
+
 const main = async () => {
   const buffer = await fs.readFile('day04/input.txt')
-  const lines = buffer.toString().split('\n').filter(Boolean)
+  const lines = buffer.toString().split('\n').map(s => s.trim()).filter(Boolean)
   const { numbers, boards } = parseLines(lines)
   part1(numbers, boards)
+  for (const board of boards) { board.reset() }
+  part2(numbers, boards)
 }
 
 main()
