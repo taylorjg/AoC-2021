43dd830c4c00a3fb87cac6b5eec79fe803f19e11
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-06 11:47:46 +0000

    Tying day 6 part 2 yet another way

--- day06/index.js
+++ day06/index.js
@@ -44,16 +44,43 @@ const part2 = (initialAges, days) => {
   console.log('Answer (part2):', runningLength)
 }
 
+const fred = (item, days, length) => {
+  // console.log({ item, days, length })
+  for (const day of R.range(item.day, days)) {
+    if (item.age === 0) {
+      item.age = 6
+      length = fred({ age: 8, day: day + 1 }, days, length + 1)
+    } else {
+      item.age -= 1
+    }
+  }
+  return length
+}
+
+const part2New = (initialAges, days) => {
+  const initialItems = initialAges.map(age => ({ age, day: 0 }))
+  let growth = 0
+  for (const item of initialItems) {
+    growth += fred(item, days, 0)
+  }
+  console.log('Answer (part2):', initialAges.length + growth)
+}
+
 const main = async () => {
-  // const buffer = await fs.readFile('day06/example.txt')
-  const buffer = await fs.readFile('day06/input.txt')
+  const buffer = await fs.readFile('day06/example.txt')
+  // const buffer = await fs.readFile('day06/input.txt')
   const initialAges = buffer.toString().split('\n')[0].split(',').map(Number)
+
   part1(initialAges, 18)
   part2(initialAges, 18)
+  part2New(initialAges, 18)
+
   part1(initialAges, 80)
   part2(initialAges, 80)
-  // part1(initialAges, 256)
-  // part2(initialAges, 256)
+  part2New(initialAges, 80)
+
+  part2(initialAges, 256)
+  // part2New(initialAges, 256)
 }
 
 main()
