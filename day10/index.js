40656300764c7b106e3b0ad99589c34bdb8ab2d4
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-10 10:54:32 +0000

    Day 10 (part 2)

--- day10/index.js
+++ day10/index.js
@@ -1,13 +1,6 @@
 const fs = require('fs').promises
 const R = require('ramda')
 
-const TABLE = new Map([
-  [')', 3],
-  [']', 57],
-  ['}', 1197],
-  ['>', 25137]
-])
-
 const PAIRS = new Map([
   ['(', ')'],
   ['[', ']'],
@@ -36,14 +29,46 @@ const validateChunk = chunk => {
       }
     }
   }
-  return { ok: true }
+  return { ok: true, chunk, stack }
 }
 
 const part1 = (chunks) => {
+  const TABLE = new Map([
+    [')', 3],
+    [']', 57],
+    ['}', 1197],
+    ['>', 25137]
+  ])
+
   const results = chunks.map(validateChunk)
   const badResults = results.filter(({ ok }) => !ok)
   const answer = badResults.reduce((acc, r) => acc + TABLE.get(r.firstIllegalChar), 0)
   console.log('Answer (part1):', answer)
+}
+
+const part2 = (chunks) => {
+  const TABLE = new Map([
+    [')', 1],
+    [']', 2],
+    ['}', 3],
+    ['>', 4]
+  ])
+  const results = chunks.map(validateChunk)
+  const incomplete = results.filter(({ ok }) => ok)
+  const totals = []
+  for (const r of incomplete) {
+    const closings = r.stack.slice().reverse()
+    let total = 0
+    for (const closing of closings) {
+      total *= 5
+      total += TABLE.get(closing)
+    }
+    totals.push(total)
+  }
+  const sorted = totals.sort((a, b) => a - b)
+  const middle = Math.floor(sorted.length / 2)
+  const answer = sorted[middle]
+  console.log('Answer (part2):', answer)
 }
 
 const main = async () => {
@@ -51,6 +76,7 @@ const main = async () => {
   const buffer = await fs.readFile('day10/input.txt')
   const chunks = buffer.toString().split('\n').filter(Boolean)
   part1(chunks)
+  part2(chunks)
 }
 
 main()
