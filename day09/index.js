c38e7c7434b9d0e33eb5c7b87fa0f6c42012640d
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-10 14:17:36 +0000

    Day 9 (part 2)

--- day09/index.js
+++ day09/index.js
@@ -2,41 +2,92 @@ const fs = require('fs').promises
 const R = require('ramda')
 
 const sum = xs => xs.reduce((a, b) => a + b, 0)
+const product = xs => xs.reduce((a, b) => a * b, 1)
 
 const within = (minInclusive, maxExclusive, v) => v >= minInclusive && v < maxExclusive
 
-const part1 = (heightmap) => {
-
+const configureGetNeighboursCoords = heightmap => {
   const numRows = heightmap.length
   const numCols = heightmap[0].length
-
-  const getNeighboursCoords = (row, col) => {
+  return (row, col) => {
     const offsets = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     const neighboursCoords = []
     for (const [rowOffset, colOffset] of offsets) {
       const r = row + rowOffset
       const c = col + colOffset
       if (within(0, numRows, r) && within(0, numCols, c)) {
-        neighboursCoords.push({ row: r, col: c })
+        neighboursCoords.push({ row: r, col: c, v: heightmap[r][c] })
       }
     }
     return neighboursCoords
   }
+}
 
+const findLowpoints = heightmap => {
+
+  const getNeighboursCoords = configureGetNeighboursCoords(heightmap)
+
+  const numRows = heightmap.length
+  const numCols = heightmap[0].length
   const lowpoints = []
+
   for (const row of R.range(0, numRows)) {
     for (const col of R.range(0, numCols)) {
-      const neighboursCoords = getNeighboursCoords(row, col)
       const v = heightmap[row][col]
-      const isLowpoint = neighboursCoords.every(({ row: r, col: c }) => v < heightmap[r][c])
+      const ns = getNeighboursCoords(row, col)
+      const isLowpoint = ns.every(n => v < n.v)
       if (isLowpoint) {
         lowpoints.push({ row, col, v })
       }
     }
   }
-  console.dir(lowpoints)
+
+  return lowpoints
+}
+
+const part1 = heightmap => {
+  const lowpoints = findLowpoints(heightmap)
   const answer = sum(lowpoints.map(({ v }) => v + 1))
   console.log('Answer (part1):', answer)
+}
+
+const findBasin = (heightmap, lowpoint) => {
+
+  const getNeighboursCoords = configureGetNeighboursCoords(heightmap)
+
+  const makeKey = ({ row, col }) => `${row}:${col}`
+  const seen = new Set([makeKey(lowpoint)])
+  const queue = [lowpoint]
+  const basin = [lowpoint]
+
+  for (; ;) {
+    if (queue.length === 0) break
+    const current = queue.shift()
+    const { row, col } = current
+    const ns = getNeighboursCoords(row, col)
+    for (const n of ns) {
+      const k = makeKey(n)
+      if (!seen.has(k)) {
+        seen.add(k)
+        if (n.v < 9) {
+          basin.push(n)
+          queue.push(n)
+        }
+      }
+    }
+  }
+
+  return basin
+}
+
+const part2 = heightmap => {
+  const lowpoints = findLowpoints(heightmap)
+  const basins = lowpoints.map(lowpoint => findBasin(heightmap, lowpoint))
+  const basinSizes = basins.map(basin => basin.length)
+  const basinSizesSorted = basinSizes.sort((a, b) => b - a)
+  const top3 = basinSizesSorted.slice(0, 3)
+  const answer = product(top3)
+  console.log('Answer (part2):', answer)
 }
 
 const main = async () => {
@@ -45,6 +96,7 @@ const main = async () => {
   const lines = buffer.toString().split('\n').filter(Boolean)
   const heightmap = lines.map(line => Array.from(line).map(Number))
   part1(heightmap)
+  part2(heightmap)
 }
 
 main()
