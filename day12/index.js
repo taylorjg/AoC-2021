ca6a9aedc8d1cd583730acbf758cb9ca5529fc48
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-12 19:20:18 +0000

    Day 12 (part 2 - almost)

--- day12/index.js
+++ day12/index.js
@@ -32,7 +32,7 @@ const part1 = (vs, am) => {
       const pns2 = pns.slice()
       pns2.push(jn)
       if (jn === 'end') {
-        const fullPathName = pns2.join('-')
+        const fullPathName = pns2.join(',')
         paths.add(fullPathName)
       } else {
         fred(j, pis2, pns2)
@@ -48,11 +48,67 @@ const part1 = (vs, am) => {
   console.log('Answer (part1):', paths.size)
 }
 
+const part2 = (vs, am) => {
+
+  const paths = new Set()
+
+  const fred = (i, pis, pns, twoVisitCave) => {
+    const js = am[i].flatMap((bit, index) => bit ? [index] : [])
+    for (j of js) {
+      const jn = vs[j]
+      if (isSmallCave(jn)) {
+        if (twoVisitCave) {
+          if (jn === twoVisitCave) {
+            const count = pns.filter(n => n === jn).length
+            if (count >= 2) {
+              continue
+            }
+          } else {
+            if (pns.includes(jn)) {
+              continue
+            }
+          }
+        } else {
+          if (pns.includes(jn)) {
+            continue
+          }
+        }
+      }
+      const pis2 = pis.slice()
+      pis2.push(j)
+      const pns2 = pns.slice()
+      pns2.push(jn)
+      if (jn === 'end') {
+        const fullPathName = pns2.join(',')
+        paths.add(fullPathName)
+      } else {
+        if (twoVisitCave) {
+          fred(j, pis2, pns2, twoVisitCave)
+        } else {
+          if (isSmallCave(jn) && jn !== 'start') {
+            // fred(j, pis2, pns2)
+            fred(j, pis2, pns2, jn)
+          } else {
+            fred(j, pis2, pns2)
+          }
+        }
+      }
+    }
+  }
+
+  const startIndex = vs.findIndex(v => v === 'start')
+  fred(startIndex, [startIndex], ['start'])
+
+  console.dir(paths)
+
+  console.log('Answer (part2):', paths.size)
+}
+
 const main = async () => {
-  // const buffer = await fs.readFile('day12/example1.txt')
+  const buffer = await fs.readFile('day12/example1.txt')
   // const buffer = await fs.readFile('day12/example2.txt')
   // const buffer = await fs.readFile('day12/example3.txt')
-  const buffer = await fs.readFile('day12/input.txt')
+  // const buffer = await fs.readFile('day12/input.txt')
   const lines = buffer.toString().split('\n').filter(Boolean)
   const edges = lines.map(line => line.split('-')).map(([from, to]) => ({ from, to }))
   console.dir(edges)
@@ -62,7 +118,8 @@ const main = async () => {
   console.dir(vertices)
   const am = buildAdjacencyMatrix(vertices, edges)
   console.dir(am)
-  part1(vertices, am)
+  // part1(vertices, am)
+  part2(vertices, am)
 }
 
 main()
