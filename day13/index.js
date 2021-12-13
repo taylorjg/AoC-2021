0de2054a5fbcb74252b57e069cb6a46d20cedc5b
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-13 10:38:41 +0000

    Day 13 - a bit of tidy up

--- day13/index.js
+++ day13/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises
 const R = require('ramda')
 
-const foldUp = (matrix1, alongY) => {
+const foldUp = matrix1 => {
   const numRows1 = matrix1.length
   const numCols1 = matrix1[0].length
   const numRows2 = Math.floor(numRows1 / 2)
@@ -22,7 +22,7 @@ const foldUp = (matrix1, alongY) => {
   return matrix2
 }
 
-const foldLeft = (matrix1, alongX) => {
+const foldLeft = matrix1 => {
   const numRows1 = matrix1.length
   const numCols1 = matrix1[0].length
   const numRows2 = numRows1
@@ -60,7 +60,6 @@ const countOnes = matrix => {
 const part1 = (dots, folds) => {
   const maxx = Math.max(...dots.map(dot => dot.x))
   const maxy = Math.max(...dots.map(dot => dot.y))
-  console.dir({ maxx, maxy })
   const numRows = maxy + 1
   const numCols = maxx + 1
   let matrix = R.range(0, numRows).map(_ => Array(numCols).fill(0, 0, numCols))
@@ -68,11 +67,7 @@ const part1 = (dots, folds) => {
     matrix[y][x] = 1
   }
   const fold = folds[0]
-  if (fold.y !== undefined) {
-    matrix = foldUp(matrix, fold.y)
-  } else {
-    matrix = foldLeft(matrix, fold.x)
-  }
+  matrix = fold.y ? foldUp(matrix) : foldLeft(matrix)
   const ones = countOnes(matrix)
   console.log('Answer (part1):', ones)
 }
@@ -80,7 +75,6 @@ const part1 = (dots, folds) => {
 const part2 = (dots, folds) => {
   const maxx = Math.max(...dots.map(dot => dot.x))
   const maxy = Math.max(...dots.map(dot => dot.y))
-  console.dir({ maxx, maxy })
   const numRows = maxy + 1
   const numCols = maxx + 1
   let matrix = R.range(0, numRows).map(_ => Array(numCols).fill(0, 0, numCols))
@@ -88,15 +82,29 @@ const part2 = (dots, folds) => {
     matrix[y][x] = 1
   }
   for (const fold of folds) {
-    if (fold.y !== undefined) {
-      matrix = foldUp(matrix, fold.y)
-    } else {
-      matrix = foldLeft(matrix, fold.x)
-    }
+    matrix = fold.y ? foldUp(matrix) : foldLeft(matrix)
   }
-
   for (const row of matrix) {
     console.log(row.map(n => n ? 'x' : ' ').join(''))
+  }
+}
+
+const parseDot = line => {
+  const bits = line.split(',')
+  return {
+    x: Number(bits[0]),
+    y: Number(bits[1])
+  }
+}
+
+const parseFold = line => {
+  const bits1 = line.split(/\s+/)
+  const bits2 = bits1[2].split('=')
+  if (bits2[0] === 'x') {
+    return { x: Number(bits2[1]) }
+  }
+  if (bits2[0] === 'y') {
+    return { y: Number(bits2[1]) }
   }
 }
 
@@ -105,28 +113,8 @@ const main = async () => {
   const buffer = await fs.readFile('day13/input.txt')
   const lines = buffer.toString().split('\n').filter(Boolean)
   const splitIndex = lines.findIndex(line => line.startsWith('fold'))
-  const dots = lines.slice(0, splitIndex).map(line => {
-    const bits = line.split(',')
-    return {
-      x: Number(bits[0]),
-      y: Number(bits[1])
-    }
-  })
-  const folds = lines.slice(splitIndex).map(line => {
-    const bits1 = line.split(/\s+/)
-    if (bits1.length === 3) {
-      const bits2 = bits1[2].split('=')
-      if (bits2.length === 2) {
-        if (bits2[0] === 'x') {
-          return { x: Number(bits2[1]) }
-        }
-        if (bits2[0] === 'y') {
-          return { y: Number(bits2[1]) }
-        }
-      }
-    }
-    throw new Error(`failed to parse fold line: ${line}`)
-  })
+  const dots = lines.slice(0, splitIndex).map(parseDot)
+  const folds = lines.slice(splitIndex).map(parseFold)
   part1(dots, folds)
   part2(dots, folds)
 }
