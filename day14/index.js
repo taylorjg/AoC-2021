3e3de29fef89e24686bdda521ef5a2c96546c4f5
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-14 18:06:35 +0000

    Day 14 (part 1)

--- day14/index.js
+++ day14/index.js
@@ -3,7 +3,7 @@ const R = require('ramda')
 
 const sum = xs => xs.reduce((a, b) => a + b, 0)
 
-const splitIntoPairCounts = s => {
+const initialisePairCounts = s => {
   const pairCounts = new Map()
   for (const index of R.range(0, s.length - 1)) {
     const pair = s.slice(index, index + 2)
@@ -14,7 +14,17 @@ const splitIntoPairCounts = s => {
   return pairCounts
 }
 
-const applyRules = (pairCounts, rules) => {
+const initialiseCharCounts = s => {
+  const charCounts = new Map()
+  for (const ch of Array.from(s)) {
+    const currentCount = charCounts.get(ch) ?? 0
+    const newCount = currentCount + 1
+    charCounts.set(ch, newCount)
+  }
+  return charCounts
+}
+
+const applyRules = (pairCounts, charCounts, rules) => {
   const adjustments = []
   for (const { pair, insert } of rules) {
     if (pairCounts.has(pair)) {
@@ -24,6 +34,9 @@ const applyRules = (pairCounts, rules) => {
       pairCounts.delete(pair)
       adjustments.push({ pair: A + C, count })
       adjustments.push({ pair: C + B, count })
+      const currentCharCount = charCounts.get(C) ?? 0
+      const newCharCount = currentCharCount + count
+      charCounts.set(C, newCharCount)
     }
   }
   for (const { pair, count } of adjustments) {
@@ -31,29 +44,21 @@ const applyRules = (pairCounts, rules) => {
     const newCount = currentCount + count
     pairCounts.set(pair, newCount)
   }
-  return pairCounts
 }
 
 const part1 = (template, rules, steps) => {
-  let pairCounts = splitIntoPairCounts(template)
+  let pairCounts = initialisePairCounts(template)
+  let charCounts = initialiseCharCounts(template)
   for (const _ of R.range(0, steps)) {
-    pairCounts = applyRules(pairCounts, rules)
+    applyRules(pairCounts, charCounts, rules)
   }
   console.dir(pairCounts)
-  const totalPairs = sum(Array.from(pairCounts.values()))
-  console.log('totalPairs:', totalPairs)
-  const charCounts = new Map()
-  for (const [pair, count] of pairCounts) {
-    const [A, B] = pair
-    const currentCharCountA = charCounts.get(A) ?? 0
-    const newCharCountA = currentCharCountA + count
-    charCounts.set(A, newCharCountA)
-    const currentCharCountB = charCounts.get(B) ?? 0
-    const newCharCountB = currentCharCountB + count
-    charCounts.set(B, newCharCountB)
-  }
   console.dir(charCounts)
-  // console.log('Answer (part1):', 0)
+  const sortedCounts = Array.from(charCounts.values()).sort((a, b) => b - a)
+  const mostCommon = R.head(sortedCounts)
+  const leastCommon = R.last(sortedCounts)
+  const answer = mostCommon - leastCommon
+  console.log('Answer (part1):', answer)
 }
 
 const parseRule = line => {
@@ -65,8 +70,8 @@ const parseRule = line => {
 }
 
 const main = async () => {
-  const buffer = await fs.readFile('day14/example.txt')
-  // const buffer = await fs.readFile('day14/input.txt')
+  // const buffer = await fs.readFile('day14/example.txt')
+  const buffer = await fs.readFile('day14/input.txt')
   const lines = buffer.toString().split('\n').filter(Boolean)
   const template = lines.shift()
   const rules = lines.map(parseRule)
