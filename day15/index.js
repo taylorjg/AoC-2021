30745c14c000360facd78bb1327bca8f34acf0e4
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-15 11:12:34 +0000

    Day 15 - part 1 refactoring before part 2

--- day15/index.js
+++ day15/index.js
@@ -23,10 +23,8 @@ const reconstructPath = (cameFrom, currentKey) => {
   return totalPath.reverse().map(parseKey)
 }
 
-const getNeighbours = (grid, key) => {
+const getNeighbours = (key, numRows, numCols) => {
   const { row, col } = parseKey(key)
-  const numRows = grid.length
-  const numCols = grid[0].length
   const offsets = [[-1, 0], [+1, 0], [0, -1], [0, +1]]
   const ns = []
   for (const [rowOffset, colOffset] of offsets) {
@@ -39,38 +37,38 @@ const getNeighbours = (grid, key) => {
   return ns
 }
 
-const A_star = (grid, start, goal) => {
+const A_star = (numRows, numCols, gridLookup, start, goal) => {
   const startKey = makeKey(start.row, start.col)
   const goalKey = makeKey(goal.row, goal.col)
   const openSet = new Set([startKey])
   const cameFrom = new Map()
   const gScores = new Map([[startKey, 0]])
+  const gScoresGet = k => gScores.get(k) ?? Number.MAX_SAFE_INTEGER
   const fScores = new Map([[startKey, manhattanDistance(start, goal)]])
+  const fScoresGet = k => fScores.get(k) ?? Number.MAX_SAFE_INTEGER
 
   for (; ;) {
     if (openSet.length === 0) break
-    const fs = Array.from(openSet.values()).map(k => ({ k, f: fScores.get(k) ?? Number.MAX_SAFE_INTEGER }))
-    const fs_sorted = fs.sort((a, b) => a.f - b.f)
-    const currentKey = fs_sorted[0].k
+    const openSetWithScores = Array.from(openSet.values()).map(k => ({ k, f: fScoresGet(k) }))
+    openSetWithScores.sort((a, b) => a.f - b.f)
+    const currentKey = R.head(openSetWithScores).k
     if (currentKey === goalKey) {
       return reconstructPath(cameFrom, currentKey)
     }
 
     openSet.delete(currentKey)
-    for (const n of getNeighbours(grid, currentKey)) {
+    for (const n of getNeighbours(currentKey, numRows, numCols)) {
       const nkey = makeKey(n.row, n.col)
-      const gScore = gScores.get(currentKey)
-      const weight = grid[n.row][n.col]
+      const gScore = gScoresGet(currentKey)
+      const weight = gridLookup(n)
       const tentativeScoreNeighbour = gScore + weight
-      const currentScoreNeighbour = gScores.get(nkey) ?? Number.MAX_SAFE_INTEGER
+      const currentScoreNeighbour = gScoresGet(nkey)
       if (tentativeScoreNeighbour < currentScoreNeighbour) {
         cameFrom.set(nkey, currentKey)
         gScores.set(nkey, tentativeScoreNeighbour)
         const hScore = manhattanDistance(n, goal)
         fScores.set(nkey, tentativeScoreNeighbour + hScore)
-        if (!openSet.has(nkey)) {
-          openSet.add(nkey)
-        }
+        openSet.add(nkey)
       }
     }
   }
@@ -83,17 +81,18 @@ const part1 = grid => {
   const numCols = grid[0].length
   const START = { row: 0, col: 0 }
   const GOAL = { row: numRows - 1, col: numCols - 1 }
-  const path = A_star(grid, START, GOAL)
+  const gridLookup = location => grid[location.row][location.col]
+  const path = A_star(numRows, numCols, gridLookup, START, GOAL)
   console.dir(path)
-  const values = path.map(({ row, col }) => grid[row][col])
+  const values = path.map(gridLookup)
   console.dir(values)
   const answer = sum(values.slice(1))
   console.log('Answer (part1):', answer)
 }
 
 const main = async () => {
-  // const buffer = await fs.readFile('day15/example.txt')
-  const buffer = await fs.readFile('day15/input.txt')
+  const buffer = await fs.readFile('day15/example.txt')
+  // const buffer = await fs.readFile('day15/input.txt')
   const lines = buffer.toString().split('\n').filter(Boolean)
   const grid = lines.map(line => Array.from(line).map(Number))
   part1(grid)
