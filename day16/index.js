19aab47d8e0eb46114affb9bfb5467e9e7f92e30
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-16 10:29:39 +0000

    Day 16 (part 1)

--- day16/index.js
+++ day16/index.js
@@ -3,14 +3,14 @@ const R = require('ramda')
 
 const sum = xs => xs.reduce((a, b) => a + b, 0)
 
-const hexCharToInt = ch => parseInt(ch, 16)
-
 const intToBinaryString = n => n.toString(2).padStart(4, '0')
 
-const hexStringToBinaryString = s =>
-  Array.from(s).map(ch => intToBinaryString(hexCharToInt(ch))).join('')
+const hexCharToInt = hexChar => parseInt(hexChar, 16)
 
-const parseLiteralPacket = binaryString => {
+const hexStringToBinaryString = hexString =>
+  Array.from(hexString).map(hexChar => intToBinaryString(hexCharToInt(hexChar))).join('')
+
+const parseLiteralPacket = (binaryString, version, typeId) => {
   let numberString = ''
   let currentIndex = 6
   for (; ;) {
@@ -21,72 +21,81 @@ const parseLiteralPacket = binaryString => {
     if (fiveBits[0] === '0') break
   }
   const literal = parseInt(numberString, 2)
-  return { literal, length: currentIndex }
+  return { version, typeId, length: currentIndex, literal }
 }
 
-const parseOperatorPacket = binaryString => {
+const parseOperatorPacket = (binaryString, version, typeId) => {
   const lengthBit = binaryString.slice(6, 7)
   if (lengthBit === '0') {
     const lengthInBits = parseInt(binaryString.slice(7, 7 + 15), 2)
-    console.log('operator', { lengthBit, lengthInBits })
+    // console.log('operator', { lengthBit, lengthInBits })
     let remainder = binaryString.slice(7 + 15, 7 + 15 + lengthInBits)
+    const packets = []
     for (; ;) {
-      const len = parsePacket(remainder)
-      remainder = remainder.slice(len)
+      const packet = parsePacket(remainder)
+      packets.push(packet)
+      remainder = remainder.slice(packet.length)
       if (remainder === '' || Array.from(remainder).every(ch => ch === '0')) {
         break
       }
     }
-    return 7 + 15 + lengthInBits
+    return { version, typeId, length: 7 + 15 + lengthInBits, packets }
   } else {
     const lengthInSubPackets = parseInt(binaryString.slice(7, 7 + 11), 2)
-    console.log('operator', { lengthBit, lengthInSubPackets })
+    // console.log('operator', { lengthBit, lengthInSubPackets })
     let lengthInBits = 0
     let remainder = binaryString.slice(7 + 11)
+    const packets = []
     for (; ;) {
-      const len = parsePacket(remainder)
-      lengthInBits += len
-      remainder = remainder.slice(len)
+      const packet = parsePacket(remainder)
+      packets.push(packet)
+      lengthInBits += packet.length
+      remainder = remainder.slice(packet.length)
       if (remainder === '' || Array.from(remainder).every(ch => ch === '0')) {
         break
       }
     }
-    return 7 + 11 + lengthInBits
+    return { version, typeId, length: 7 + 11 + lengthInBits, packets }
   }
 }
 
 const parsePacket = binaryString => {
   const version = parseInt(binaryString.slice(0, 3), 2)
   const typeId = parseInt(binaryString.slice(3, 6), 2)
-  console.log({ version, typeId })
+  // console.log({ version, typeId })
   if (typeId === 4) {
-    const { literal, length } = parseLiteralPacket(binaryString)
-    console.log('literal', { literal, length })
-    return length
+    const packet = parseLiteralPacket(binaryString, version, typeId)
+    return packet
   } else {
-    return parseOperatorPacket(binaryString)
+    return parseOperatorPacket(binaryString, version, typeId)
   }
 }
 
-const part1 = packet => {
-  let binaryString = hexStringToBinaryString(packet)
+const part1 = hexString => {
+  let binaryString = hexStringToBinaryString(hexString)
   console.log(binaryString)
-  parsePacket(binaryString)
-  console.log('Answer (part1):', 0)
+  const packet = parsePacket(binaryString)
+  // console.dir(packet, { depth: null })
+  const versions = []
+  const extractVersions = p => {
+    const { version, packets = [] } = p
+    versions.push(version)
+    packets.forEach(extractVersions)
+  }
+  extractVersions(packet)
+  console.dir(versions)
+  const answer = sum(versions)
+  console.log('Answer (part1):', answer)
 }
 
 const main = async () => {
   const buffer = await fs.readFile('day16/input.txt')
-  const packet = buffer.toString().split('\n')[0]
-  console.dir(packet)
-  // part1('D2FE28')
-  // part1('38006F45291200')
-  // part1('EE00D40C823060')
-  part1('8A004A801A8002F478')
+  const hexString = buffer.toString().split('\n')[0]
+  // part1('8A004A801A8002F478')
   // part1('620080001611562C8802118E34')
   // part1('C0015000016115A2E0802F182340')
   // part1('A0016C880162017C3686B18A3D4780')
-  // part1(packet)
+  part1(hexString)
 }
 
 main()
