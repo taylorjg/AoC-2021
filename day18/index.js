b89f0bbafc9368e747e0548d333de94e54ba044e
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-18 23:12:13 +0000

    Day 18 (explode working)

--- day18/index.js
+++ day18/index.js
@@ -1,12 +1,134 @@
 const fs = require('fs').promises
 
-const explode = () => {
+const isRoot = n => !n.parent
+const isRegular = n => Number.isInteger(n)
+const isNode = n => !isRegular(n)
+
+const findNodeWithRegular = n => {
+  if (isRegular(n.left) || isRegular(n.right)) {
+    return n
+  }
+  if (isNode(n.left)) {
+    const n2 = findNodeWithRegular(n.left)
+    if (n2) return n2
+  }
+  if (isNode(n.right)) {
+    const n2 = findNodeWithRegular(n.right)
+    if (n2) return n2
+  }
+  return null
 }
 
-const split = () => {
+const canReachNodeFollowingLeftPath = (n1, n2) => {
+  if (isRegular(n1.left)) return false
+  if (n1.left === n2) return true
+  return canReachNodeFollowingLeftPath(n1.left, n2)
 }
 
+const canReachNodeFollowingRightPath = (n1, n2) => {
+  if (isRegular(n1.right)) return false
+  if (n1.right === n2) return true
+  return canReachNodeFollowingRightPath(n1.right, n2)
+}
+
+const findBranchPointLeft = n => {
+  let current = n
+  for (; ;) {
+    if (isRoot(current)) {
+      if (canReachNodeFollowingLeftPath(current, n)) return null
+      return current
+    }
+    current = current.parent
+    if (!canReachNodeFollowingLeftPath(current, n)) return current
+  }
+}
+
+const findBranchPointRight = n => {
+  let current = n
+  for (; ;) {
+    if (isRoot(current)) {
+      if (canReachNodeFollowingRightPath(current, n)) return null
+      return current
+    }
+    current = current.parent
+    if (!canReachNodeFollowingRightPath(current, n)) return current
+  }
+}
+
+const findLeftNodeWithRegular = n => {
+  const branchNode = findBranchPointLeft(n)
+  if (!branchNode) return null
+  if (isRegular(branchNode.left)) return branchNode
+  return findNodeWithRegular(branchNode.left)
+}
+
+const findRightNodeWithRegular = n => {
+  const branchNode = findBranchPointRight(n)
+  if (!branchNode) return null
+  if (isRegular(branchNode.right)) return branchNode
+  return findNodeWithRegular(branchNode.right)
+}
+
+const explode = number => {
+
+  const leftNodeWithRegular = findLeftNodeWithRegular(number)
+  const rightNodeWithRegular = findRightNodeWithRegular(number)
+
+  if (leftNodeWithRegular) {
+    if (isRegular(leftNodeWithRegular.left)) {
+      leftNodeWithRegular.left += number.left
+    } else {
+      leftNodeWithRegular.right += number.left
+    }
+  }
+
+  if (rightNodeWithRegular) {
+    if (isRegular(rightNodeWithRegular.left)) {
+      rightNodeWithRegular.left += number.right
+    } else {
+      rightNodeWithRegular.right += number.right
+    }
+  }
+
+  if (number.parent.left === number) {
+    number.parent.left = 0
+  }
+
+  if (number.parent.right === number) {
+    number.parent.right = 0
+  }
+}
+
+const split = number => {
+  const left = Math.floor(number / 2)
+  const right = Math.ceil(number / 2)
+  return { left, right }
+}
+
+// To reduce a snailfish number, you must repeatedly do the first action in
+// this list that applies to the snailfish number:
+//
+//  If any pair is nested inside four pairs, the leftmost such pair explodes.
+//
+//  If any regular number is 10 or greater, the leftmost such regular number splits.
 const reduce = number => {
+  const descend = (n, level) => {
+    if (Number.isInteger(n)) return false
+    // if (Number.isInteger(n)) {
+    //   if (n >= 10) {
+    //     // make change
+    //     return true
+    //   }
+    // }
+    if (level === 4) {
+      explode(n)
+      return true
+    } else {
+      if (descend(n.left, level + 1)) return true
+      return descend(n.right, level + 1)
+    }
+  }
+  descend(number, 0)
 }
 
 const add = (a, b) => {
@@ -20,7 +142,9 @@ const magnitude = number => {
 }
 
 const part1 = numbers => {
-  console.log('Answer (part1):', 0)
+  const total = numbers.reduce(add)
+  const answer = magnitude(total)
+  console.log('Answer (part1):', answer)
 }
 
 const parseNumber = line => {
@@ -30,6 +154,9 @@ const parseNumber = line => {
     switch (ch) {
       case '[': {
         const node = {}
+        if (nodes.length) {
+          node.parent = nodes[nodes.length - 1]
+        }
         nodes.push(node)
         const result = helper(nodes, remainder)
         node.left = result.node
@@ -57,12 +184,23 @@ const parseNumber = line => {
 }
 
 const main = async () => {
-  const buffer = await fs.readFile('day18/example1.txt')
-  // const buffer = await fs.readFile('day18/input.txt')
-  const lines = buffer.toString().split('\n').filter(Boolean)
-  const numbers = lines.map(parseNumber)
-  console.dir(numbers, { depth: null })
-  part1(numbers)
+  // const buffer = await fs.readFile('day18/example1.txt')
+  // // const buffer = await fs.readFile('day18/input.txt')
+  // const lines = buffer.toString().split('\n').filter(Boolean)
+  // const numbers = lines.map(parseNumber)
+  // console.dir(numbers, { depth: null })
+  // part1(numbers)
+  // const number = parseNumber('[[[[[9,8],1],2],3],4]')
+  // const number = parseNumber('[7,[6,[5,[4,[3,2]]]]]')
+  // const number = parseNumber('[[6,[5,[4,[3,2]]]],1]')
+  // const number = parseNumber('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]')
+  const number = parseNumber('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]')
+  // console.dir(number.right.right.right.right)
+  // explode(number.right.right.right.right)
+  reduce(number)
+  console.dir(number, { depth: null })
+  // console.dir(split(parseNumber('')))
+  // console.dir(reduce(parseNumber('')))
 }
 
 main()
