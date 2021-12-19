9782b07a059944cda077f17c14e2fe684ff8ade5
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-19 23:45:22 +0000

    Day 18 (part 1 - finally)

--- day18/index.js
+++ day18/index.js
@@ -2,43 +2,22 @@ const fs = require('fs').promises
 
 const {
   parseNumber,
-  formatNumber,
-  isRoot,
-  isRegular,
-  isNode,
-  explode,
-  reduce,
   add,
   magnitude
 } = require('./logic')
 
+const part1 = numbers => {
+  const result = numbers.reduce(add)
+  const answer = magnitude(result)
+  console.log('Answer (part1):', answer)
+}
+
 const main = async () => {
-  const buffer = await fs.readFile('day18/example1.txt')
-  // const buffer = await fs.readFile('day18/input.txt')
+  // const buffer = await fs.readFile('day18/example2.txt')
+  const buffer = await fs.readFile('day18/input.txt')
   const lines = buffer.toString().split('\n').filter(Boolean)
   const numbers = lines.map(parseNumber)
-  // numbers.forEach(n => console.log(formatNumber(n)))
-  // part1(numbers)
-  const c = add(numbers[0], numbers[1])
-  // const d = add(c, numbers[2])
-  // const e = add(d, numbers[3])
-
-  // this works
-  // const a = parseNumber('[[[[4,3],4],4],[7,[[8,4],9]]]')
-  // const b = parseNumber('[1,1]')
-  // add(a, b)
-
-  // const regulars = listRegulars(numbers[0])
-  // const v1 = []
-  // regulars.forEach(n => {
-  //   if (isRegular(n.left)) {
-  //     v1.push(n.left)
-  //   }
-  //   if (isRegular(n.right)) {
-  //     v1.push(n.right)
-  //   }
-  // })
-  // console.dir(v1)
+  part1(numbers)
 }
 
 main()
--- day18/logic.js
+++ day18/logic.js
@@ -4,20 +4,7 @@ const isNode = n => !isRegular(n)
 
 const findRoot = n => isRoot(n) ? n : findRoot(n.parent)
 
-const dumpRegulars = regulars => {
-  const values = []
-  regulars.forEach(n => {
-    if (isRegular(n.left)) {
-      values.push(n.left)
-    }
-    if (isRegular(n.right)) {
-      values.push(n.right)
-    }
-  })
-  console.log('values:', JSON.stringify(values))
-}
-
-const listRegulars = root => {
+const listNodesWithRegulars = root => {
   const regulars = []
   const helper = (n, p) => {
     if (isRegular(n)) {
@@ -33,13 +20,12 @@ const listRegulars = root => {
     }
   }
   helper(root)
-  // dumpRegulars(regulars)
   return regulars
 }
 
 const findLeftRegular = n => {
   const root = findRoot(n)
-  const regulars = listRegulars(root)
+  const regulars = listNodesWithRegulars(root)
   const index = regulars.findIndex(m => m === n)
   if (index < 1) return null
   return regulars[index - 1]
@@ -47,7 +33,7 @@ const findLeftRegular = n => {
 
 const findRightRegular = n => {
   const root = findRoot(n)
-  const regulars = listRegulars(root)
+  const regulars = listNodesWithRegulars(root)
   const index = regulars.findIndex(m => m === n)
   if (index < 0 || index >= regulars.length - 1) return null
   return regulars[index + 1]
@@ -57,9 +43,6 @@ const explode = number => {
 
   const leftNodeWithRegular = findLeftRegular(number)
   const rightNodeWithRegular = findRightRegular(number)
-
-  // console.log('leftNodeWithRegular:', leftNodeWithRegular)
-  // console.log('rightNodeWithRegular:', rightNodeWithRegular)
 
   if (leftNodeWithRegular) {
     if (isRegular(leftNodeWithRegular.right)) {
@@ -100,40 +83,41 @@ const splitRight = n => {
   n.right = { parent: n, left, right }
 }
 
-// To reduce a snailfish number, you must repeatedly do the first action in
-// this list that applies to the snailfish number:
-//
-//  If any pair is nested inside four pairs, the leftmost such pair explodes.
-//
-//  If any regular number is 10 or greater, the leftmost such regular number splits.
-const reduce = number => {
-
-  const reduceOneStep = (n, level) => {
-    if (isRegular(n)) return false
-    if (level === 4) {
-      console.log(`exploding [${n.left},${n.right}]`)
-      explode(n)
-      return true
-    }
-    if (isRegular(n.left) && n.left >= 10) {
-      splitLeft(n)
-      return true
-    }
-    if (isRegular(n.right) && n.right >= 10) {
-      splitRight(n)
-      return true
-    }
-    if (reduceOneStep(n.left, level + 1)) {
-      return true
-    }
-    return reduceOneStep(n.right, level + 1)
+const findFirstLevel4Node = root => {
+  const helper = (n, level) => {
+    if (isRegular(n)) return null
+    if (level === 4) return n
+    const n2 = helper(n.left, level + 1)
+    if (n2) return n2
+    const n3 = helper(n.right, level + 1)
+    if (n3) return n3
+    return null
   }
+  return helper(root, 0)
+}
 
+const reduce = number => {
   for (; ;) {
-    console.log('before', formatNumber(number))
-    const reduced = reduceOneStep(number, 0)
-    console.log('after', formatNumber(number))
-    console.log('-'.repeat(10))
+    let reduced = false
+    const firstLevel4Node = findFirstLevel4Node(number)
+    if (firstLevel4Node) {
+      explode(firstLevel4Node)
+      reduced = true
+    } else {
+      const nodesWithRegulars = listNodesWithRegulars(number)
+      const firstBigRegular = nodesWithRegulars.find(n => (
+        (isRegular(n.left) && n.left >= 10) ||
+        (isRegular(n.right) && n.right >= 10)
+      ))
+      if (firstBigRegular) {
+        if (isRegular(firstBigRegular.left) && firstBigRegular.left >= 10) {
+          splitLeft(firstBigRegular)
+        } else {
+          splitRight(firstBigRegular)
+        }
+        reduced = true
+      }
+    }
     if (!reduced) break
   }
 }
@@ -142,10 +126,7 @@ const add = (a, b) => {
   const c = { left: a, right: b }
   c.left.parent = c
   c.right.parent = c
-  console.log(' ', formatNumber(a))
-  console.log('+', formatNumber(b))
   reduce(c)
-  console.log('=', formatNumber(c))
   return c
 }
 
