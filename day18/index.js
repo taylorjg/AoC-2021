a27c18616b45a88efcbac14411e3a0bfecc6bcea
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-20 09:14:50 +0000

    Day 18 (part 2)

--- day18/index.js
+++ day18/index.js
@@ -8,16 +8,25 @@ const part1 = numbers => {
   console.log('Answer (part1):', answer)
 }
 
-const part2 = numbers => {
+const part2 = lines => {
+  const values = []
+  lines.slice(0, -1).forEach((line1, index) => {
+    lines.slice(index + 1).forEach((line2, index) => {
+      values.push(magnitude(add(parseNumber(line1), parseNumber(line2))))
+      values.push(magnitude(add(parseNumber(line2), parseNumber(line1))))
+    })
+  })
+  const answer = Math.max(...values)
+  console.log('Answer (part2):', answer)
 }
 
 const main = async () => {
-  const buffer = await fs.readFile('day18/example2.txt')
-  // const buffer = await fs.readFile('day18/input.txt')
+  // const buffer = await fs.readFile('day18/example2.txt')
+  const buffer = await fs.readFile('day18/input.txt')
   const lines = buffer.toString().split('\n').filter(Boolean)
   const numbers = lines.map(parseNumber)
   part1(numbers)
-  part2(numbers)
+  part2(lines)
 }
 
 main()
